a6387fceed172cac66d3a8c5dc251f2173fce866
Satyam Kumar <120768916+satyamskumar45@users.noreply.github.com>
2026-03-10 16:21:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+var typed = new Typed("#typing", {
+    strings: [
+        "Backend Developer",
+        "Java Developer",
+        "Problem Solver",
+        "Cloud Enthusiast"
+    ],
+    typeSpeed: 70,
+    backSpeed: 40,
+    loop: true
+});
+const services = {
+
+backend:{
+title:"Backend Development",
+points:[
+"REST API development",
+"Node.js backend systems",
+"Database design (MongoDB / MySQL)",
+"Authentication & security"
+]
+},
+
+cloud:{
+title:"Cloud & Deployment",
+points:[
+"Deploy applications on cloud",
+"Server setup & configuration",
+"CI/CD basics",
+"Cloud-ready application architecture"
+]
+},
+
+java:{
+title:"Java Development",
+points:[
+"Core Java programming",
+"OOP concepts",
+"Backend development with Java",
+"Scalable Java applications"
+]
+}
+
+};
